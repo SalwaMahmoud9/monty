0593bc03d4474b3aa1e01450f77d20da27e2e42b
SalwaMahmoud9 <sma.mam9@gmail.com>
2023-12-13 16:15:08 +0200

    salwa 13-12-2023

---
+++ div.c
@@ -0,0 +1,40 @@
+#include "monty.h"
+/**
+ * div - divides the top two elements of the stack.
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+void div(stack_t **head, unsigned int counter)
+{
+	stack_t *head2;
+	int length = 0, aux;
+
+	head2 = *head;
+	while (head2)
+	{
+		head2 = head2->next;
+		length++;
+	}
+	if (length < 2)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	head2 = *head;
+	if (head2->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	aux = head2->next->n / head2->n;
+	head2->next->n = aux;
+	*head = head2->next;
+	free(head2);
+}
