04e9a31389d4e9db9b7790917f902e49cac1711b
Asmaa26189 <engasmaahesham@gmail.com>
2023-12-13 17:06:47 +0200

    13-12-2023

--- execute.c
+++ execute.c
@@ -13,24 +13,12 @@ int execute(char *content, stack_t **head, unsigned int lNumber, FILE *file)
 	char *op;
 	instruction_t ints[] = {
 				{"push", push}, {"pall", pall}, {"pint", pint},
-				{"pop", pop},
-				{"swap", swap},
-				{"add", add},
-				{"nop", nop},
-				{"sub", sub},
-				{"div", divFunc},
-				{"mul", mul},
-				{"mod", mod},
-				{"pchar", pchar},
-				{"pstr", pstr},
-				{"rotl", rotl},
-				{"rotr", rotr},
-				{"queue", queue},
-				{"stack", stack},
-				{NULL, NULL}
+				{"pop", pop},{"swap", swap},{"add", add},
+				{"nop", nop},{"sub", sub},{"div", divFunc},
+				{"mul", mul},{"mod", mod},{"pchar", pchar},
+				{"pstr", pstr},{"rotl", rotl},{"rotr", rotr},
+				{"queue", queue},{"stack", stack},{NULL, NULL}
 				};
-
-
 	op = strtok(content, " \n\t");
 	if (op && op[0] == '#')
 		return (0);
@@ -44,12 +32,12 @@ int execute(char *content, stack_t **head, unsigned int lNumber, FILE *file)
 		i++;
 	}
 	if (op && ints[i].opcode == NULL)
-	{ 
+	{
 		fprintf(stderr, "L%d: unknown instruction %s\n", lNumber, op);
 		fclose(file);
 		free(content);
 		free_stack(*head);
-		exit(EXIT_FAILURE); 
+		exit(EXIT_FAILURE);
 	}
 	return (1);
 }
